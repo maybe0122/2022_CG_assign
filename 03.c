c30d6061519c8c4f8a1f33431e54d59943af17c0
kyuminKim <58509870+maybe0122@users.noreply.github.com>
2022-09-14 17:11:04 +0900

    update 03.c

--- 03.c
+++ 03.c
@@ -5,6 +5,14 @@
 #include <string.h>
 #include <stdbool.h>
 
+
+#define SWAP(a, b, type) do { \
+    type temp; \
+    temp = a;  \
+    a = b;     \
+    b = temp;  \
+} while (0)
+
 int main()
 {
 	FILE* fp = NULL;
@@ -12,10 +20,11 @@ int main()
 	char buffer[128];
 	char temp[128] = { '\0' };
 	char str_temp[10][128] = { '\0' };
-	char ch;
+	char ch, c1, c2;
 	int count = 0;
 	int index = 0;
-	bool d_flag = false;
+	int same = 0;
+	bool same_flag = true;
 
 	printf("input data file name: ");
 	scanf("%s", fname);
@@ -36,8 +45,6 @@ int main()
 		strcpy(str_temp[count], buffer);
 		printf("%s", str_temp[count]);
 		count++;
-		/*for (int i = 0; i < sizeof(buffer); i++)
-			buffer[i] = '\0';*/
 		printf("\n");
 	}
 
@@ -49,24 +56,85 @@ int main()
 		switch (ch)
 		{
 		case'd':														//reverse all sentence
-			for (int k = 0; k < sizeof(str_temp) / sizeof(str_temp[0]); k++)
+			for (unsigned int k = 0; k < sizeof(str_temp) / sizeof(str_temp[0]); k++)
 			{
-				for (int i = 0; i < strlen(str_temp[k]); i++)
-				{
-					temp[i] = str_temp[k][strlen(str_temp[k]) - (i + 1)];
-					printf("%c", temp[i]);
-				}
+				for (unsigned int i = 0; i < strlen(str_temp[k]) / 2; i++)
+					SWAP(str_temp[k][i], str_temp[k][strlen(str_temp[k]) - (i+1)], char);
+				for (unsigned int j = 0; j < strlen(str_temp[k]); j++)
+					printf("%c", str_temp[k][j]);
 				printf("\n");
 			}
-			
 			break;
 		case'e':														//
+			for (unsigned int k = 0; k < sizeof(str_temp) / sizeof(str_temp[0]); k++)
+			{
+				for (unsigned int i = 0; i < strlen(str_temp[k]); i++)
+					SWAP(str_temp[k][i], str_temp[k][strlen(str_temp[k]) - (i + 1)], char);
+				for (unsigned int j = 0; j < strlen(str_temp[k]); j++)
+					printf("%c", str_temp[k][j]);
+				printf("\n");
+			}
 			break;
 		case'f':
 			break;
 		case'g':
+			while ((ch = getchar()) != '\n') continue;
+			scanf("%c %c", &c1, &c2);
+			for (unsigned int k = 0; k < sizeof(str_temp) / sizeof(str_temp[0]); k++)
+			{
+				for (unsigned int i = 0; i < strlen(str_temp[k]); i++)
+				{
+					if (str_temp[k][i] == c1)
+						SWAP(c1, c2, char);
+				}
+				for (unsigned int j = 0; j < strlen(str_temp[k]); j++)
+					printf("%c", str_temp[k][j]);
+				printf("\n");
+			}
+
+
 			break;
 		case'h':
+			while (same_flag) 
+			{
+				for (unsigned int k = 0; k < sizeof(str_temp) / sizeof(str_temp[0]); k++)
+				{
+					char same_str[128] = { '\n' };
+					for (unsigned int i = 0; i < strlen(str_temp[k]) / 2; i++)
+					{
+						if (str_temp[k][i] == str_temp[k][strlen(str_temp[k]) - (i + 1)])
+						{
+							same_str[i] = str_temp[k][i];
+							same++;
+						}
+						else
+						{
+							same_flag = false;
+							same += same;
+							break;
+						}
+					}
+
+					for (unsigned int j = 0; j < strlen(str_temp[k]); j++)
+						printf("%c", str_temp[k][j]);
+					printf(": ");
+
+					if (same == 0)
+						printf("%d\n", same);
+					else {
+						for (unsigned int n = 0; n < strlen(same_str); n++)
+						{
+							if (same_str[n] == ' ')
+								continue;
+							else
+								printf("%c", same_str[n]);
+						}
+						printf("\n");
+					}
+					same = 0;
+				}
+				//마지막에 same_str을 초기화를 해줘야하나?
+			}
 			break;
 		case'q':
 			return 0;
